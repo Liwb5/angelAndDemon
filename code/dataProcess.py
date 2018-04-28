f5ec8bfe35a38c18945728f36999e277a7f6c069
E7 <liwb5@foxmail.com>
2018-04-28 17:56:52 +0800

    modify dataProcess.py file, add two functions to process nonNumeric data and date data

--- code/dataProcess.py
+++ code/dataProcess.py
@@ -17,26 +17,96 @@ from imblearn.combine import SMOTEENN
 import sys
 import os
 
-def read_data(trainPath, testPath, info=True):
+def read_data(trainPath, testPath):
     #读取原始数据
     trainData = pd.read_csv(trainPath, header=None)
     testData = pd.read_csv(testPath, header=None)
 
-    if(info):
-        print('trainData shape: ', trainData.shape)
-        print('testData shape: ', testData.shape)
+    print('trainData shape: ', trainData.shape)
+    print('testData shape: ', testData.shape)
 
     return trainData, testData
 
-def remove_nonnumeric(data, Thresh):
-    print("removing innumeric features: ")
-    #将非数值型数据转换成nan值
-    #coerce参数将非数字的值转换成nan，如果是ignore参数，则不处理       
-    data1 = data.apply(lambda x:pd.to_numeric(x, errors="coerce")) 
 
-    data1 = data1.dropna(axis=1, thresh=Thresh)#删除nan很多的列
-    print('data shape after remove nonnumeric columns: ', data1.shape)
-    return data1
+def process_nonNumeric(data):
+    """
+    brief：处理数据中包含的非数值类型，将其映射成one-hot形式。
+        
+    params：
+        data: DataFrame类型。包含非数值型的列，需要被处理。
+    
+    return：
+        data: DataFrame类型，删除了原始data中非数值的列
+        newData：DataFrame类型。将非数值的列映射成one-hot的形式后产生的DataFrame类型。
+    """
+    nonNumericCol = [132, 135, 138, 141, 144, 147, 150, 153, 156, 159, 162, 165, 168, 171, 174, 177, 180, 183, 186, 189, 192, 195, 198, 201, 207, 244] #手动统计的
+    
+    nonNumericData = data[nonNumericCol]#提取非数值的列
+
+    
+    newData = pd.DataFrame()
+    for col in nonNumericData.columns:
+        #将非数值的每一列修改成one-hot的形式
+        res = nonNumericData[col].value_counts()
+        for indx in res.index:
+            newData[indx] = nonNumericData[col].apply(lambda x, index: 1 if x==index else 0,
+                                                      args=(indx,))
+    #将非数值的列删除
+    data = data.drop(nonNumericCol,axis=1)
+    
+    #将其它的列中的非数值数据转换成nan值。
+    #coerce参数将非数字的值转换成nan，如果是ignore参数，则不处理
+    data = data.apply(lambda x:pd.to_numeric(x, errors="coerce"))
+    
+    print('nonNumericData shape: ', newData.shape)
+    
+    return data, newData
+
+
+def process_date(data):
+    """
+    brief: 处理数据中的日期，更改时间日期格式
+    
+    params:
+        data: DataFrame类型。包含日期的列，需要被处理。
+        
+    return: 
+        data: DataFrame类型，删除了原始data中日期所在的列
+        date: DataFrame类型。将日期所在的列按照年月日时分秒映射成6个独立的属性。
+    """
+    dateCol = 206  #时间所在的列，手动统计的
+    
+    date = pd.DataFrame()
+    date['date'] = pd.to_datetime(pd.Series(data[dateCol]), format='%Y-%m-%d-%H.%M.%S.%f')
+    
+    # 转化为6个单独的属性
+    date['Year'] = date['date'].apply(lambda x: x.year)
+    date['Month'] = date['date'].apply(lambda x: x.month)
+    date['Day'] = date['date'].apply(lambda x: x.day)
+    date['Hour'] = date['date'].apply(lambda x: x.hour)
+    date['Minute'] = date['date'].apply(lambda x: x.minute)
+    date['Second'] = date['date'].apply(lambda x: x.second)
+    
+    data = data.drop(dateCol, axis=1)
+    date = date.drop('date', axis=1)
+    print('dateData shape: ', date.shape)
+    
+    return data, date
+    
+
+def remove_NAN(data, Thresh):
+    """
+    brief: 将data的每一列中非nan值少于Thresh的列删除。
+    
+    params:
+        data: DataFrame类型。
+        Thresh: int, 非nan值少于Thresh的列将被删除
+    """
+
+    data = data.dropna(axis=1, thresh=Thresh)#删除nan很多的列
+    print('data shape after remove NAN columns: ', data.shape)
+    
+    return data
 
 def remove_cols_appear_too_much(data, frequency):
     #对于那些一列中某个元素出现次数太多的，也将这些列删除
@@ -54,6 +124,23 @@ def remove_cols_appear_too_much(data, frequency):
     print("data shape after remove_cols_appear_too_much: ",data4.shape)
     return data4
 
+def remove_nonNumeric(data, Thresh=10000):
+    """
+    brief: 将非数值型数据转换成nan值,并且删除非nan值不超过Thresh的列。
+    params: 
+        data: DataFrame类型。
+        Thresh: int, 非nan值少于Thresh的列将被删除
+    """
+    #将非数值型数据转换成nan值
+    #coerce参数将非数字的值转换成nan，如果是ignore参数，则不处理       
+    data = data.apply(lambda x:pd.to_numeric(x, errors="coerce")) 
+
+    data = data.dropna(axis=1, thresh=Thresh)#删除nan很多的列
+    print('trainData shape: ', data.shape)
+    
+    return data
+
+
 def norm(data):
     #归一化处理
     mmScaler = pp.MinMaxScaler()
@@ -62,27 +149,34 @@ def norm(data):
     return data4
 
 
-    
+#----------parameters---------------#
+
+
+Threshold = 10000           #每一列非nan值超过Thresh才会保留
+#Frequency = 100000        #每一列，某元素出现次数超过Frequency的，那一列会被丢弃
+version = '5'       
 
 
 if __name__ == "__main__":
-    #----------parameters---------------#
-    os.chdir('/home/liwb/Documents/projects/angelAndDemon/')
     
-    trainPath = "./originalDataset/train.csv"
-    testPath = "./originalDataset/test.csv"
-    
-    Threshold = 10000           #每一列非nan值超过Thresh才会保留
-    Frequency = 100000        #每一列，某元素出现次数超过Frequency的，那一列会被丢弃
-    version = '5'            #数据处理后保存成文件的文件版本号
-    version = sys.argv[1]
+    path = os.path.dirname(__file__) #获得本文件所在的目录
+    if path != "":
+        os.chdir(path) #将当前路径设置为本文件所在的目录，方便下面读取文件。
 
+    if len(sys.argv) >= 2:
+        #数据处理后保存成文件的文件版本号     
+        version = sys.argv[1]
+    
+
+    trainPath = "../originalDataset/train.csv"
+    testPath = "../originalDataset/test.csv"    
+    
     trainData, testData = read_data(trainPath, testPath)
 
     featuresNum = trainData.shape[1]#得到列数
     trainNum = trainData.shape[0]#得到行数
 
-    #为了添加列名，只好先转成numpy在转回dataframe
+    #为了添加列名，只好先转成numpy再转回dataframe
     trainData = trainData.values
     testData = testData.values
 
@@ -95,51 +189,43 @@ if __name__ == "__main__":
     data = pd.DataFrame(data)
     print('merge data shape: ',data.shape)
     
-    #删除非数值的列
-    data = remove_nonnumeric(data, Threshold)
+    
+    data, nonNumericData = process_nonNumeric(data)
+
+    data, dateData = process_date(data)
+    
+    #删除NAN值很多的列
+    data = remove_NAN(data, Threshold)
 
     #缺失值填充
-    data = data.fillna(data.mean()) #对nan值用该列的均值补全。
+    #data = data.fillna(data.mean()) #对nan值用该列的均值补全。
 
     #删除某个元素出现次数太多的列
-    data = remove_cols_appear_too_much(data, Frequency)
+    #data = remove_cols_appear_too_much(data, Frequency)
 
     #归一化处理
-    data = norm(data)
+    #data = norm(data)
+    
+    #合并所有的数据
+    data = np.hstack((data.values, nonNumericData.values, dateData.values))
+    print('merge data, nonNumericData and dateData: ',data.shape)
 
-
+    
     testRes = data[trainNum:]
     trainRes = data[0:trainNum]
     print("testRes shape:",testRes.shape)
     print("trainRes shape:",trainRes.shape)
+    
     #保存测试集
     testRes = pd.DataFrame(testRes)
-    testRes.to_csv('./dataAfterProcess/testRes%s.csv'%(version), header=False, index=False, encoding='utf-8')
+    testRes.to_csv('../dataAfterProcess/testRes%s.csv'%(version), header=False, index=False, encoding='utf-8')
     
     #保存训练集
     trainRes1 = np.hstack((trainRes, label))
     trainRes2 = pd.DataFrame(trainRes1)
-    trainRes2.to_csv('./dataAfterProcess/trainRes%s.csv'%(version), header=False, index=False, encoding='utf-8')
+    trainRes2.to_csv('../dataAfterProcess/trainRes%s.csv'%(version), header=False, index=False, encoding='utf-8')
 
-    #分割验证集
-    #print("trainRes shape after merge label: ",trainRes1.shape)
-    #np.random.shuffle(trainRes1)
-
-    #validRes = trainRes1[0:20000]
-    #trainRes2 = trainRes1[20000:]
-    #print("validRes shape: ",validRes.shape, "trainRes2 shape: ",trainRes2.shape)
-
-    #保存验证集
-    #validRes = pd.DataFrame(validRes)
-    #validRes.to_csv('./dataAfterProcess/validRes%s.csv'%(version), header=False, index=False, encoding='utf-8')
 
     
-
-
-
-
-
-
-
 
 
